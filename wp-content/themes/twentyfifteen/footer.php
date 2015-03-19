83969525607766b992524acb538f1a8f1660c1e4
Todd Dumas <todd@dd9.com>
2015-03-19 15:19:36 -0600

    baby commit to force push

--- wp-content/themes/twentyfifteen/footer.php
+++ wp-content/themes/twentyfifteen/footer.php
@@ -22,7 +22,7 @@
 				 */
 				do_action( 'twentyfifteen_credits' );
 			?>
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyfifteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfifteen' ), 'WordPress' ); ?></a>
+			<a href="http://dd9.com">DD9.com</a>
 		</div><!-- .site-info -->
 	</footer><!-- .site-footer -->
 
